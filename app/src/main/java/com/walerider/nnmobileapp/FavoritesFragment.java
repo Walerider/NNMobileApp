0fd0953ee635b00098235d3a3863487869a86136
Walerider <walerider.killeron@yandex.ru>
2024-03-12 20:04:16 +0400

    12/03

--- app/src/main/java/com/walerider/nnmobileapp/FavoritesFragment.java
+++ app/src/main/java/com/walerider/nnmobileapp/FavoritesFragment.java
@@ -1,43 +1,25 @@
 package com.walerider.nnmobileapp;
 
-import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.walerider.nnmobileapp.placeholder.PlaceholderContent;
-
-/**
- * A fragment representing a list of Items.
- */
 public class FavoritesFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+    private SQLiteDatabase db;
     public FavoritesFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static FavoritesFragment newInstance(int columnCount) {
+
+    public static FavoritesFragment newInstance(String param1, String param2) {
         FavoritesFragment fragment = new FavoritesFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -45,27 +27,13 @@ public class FavoritesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_favorites_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_favorites, container, false);;
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new FavoritePlaceRecyclerViewAdapter(PlaceholderContent.ITEMS));
-        }
-        return view;
+        return v;
     }
 }
--- app/src/main/java/com/walerider/nnmobileapp/MainActivity.java
+++ app/src/main/java/com/walerider/nnmobileapp/MainActivity.java
@@ -1,21 +1,67 @@
 package com.walerider.nnmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.mapview.MapView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
     MapView mapView;
+    protected FragmentContainerView fragmentContainerView;
+    private NavHostFragment navHostFragment;
+    private SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapKitFactory.setApiKey("5b6aeb4b-f09d-4162-8f64-9068e24cbced");
-        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
-
+        db = getBaseContext().openOrCreateDatabase("app.db",MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(login TEXT,email TEXT, password TEXT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS positions(name TEXT,coordinates TEXT PRIMARY KEY)");
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+    protected void favoritesFileOpen(){
+        try {
+            InputStream inputStream = this.openFileInput("favorites.txt");
+            Log.e("firstStart","not a first start");
+            Log.e("firstStart", this.getPackageResourcePath());
+            inputStream.close();
+        }catch (FileNotFoundException e){
+            Log.e("firstStart", "file Not Found" + e);
+            writeToFile("feg",this);
+        } catch (IOException e) {
+            Log.e("firstStart", "Can not read file: " + e);
+        }
+    }
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("start.txt", MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e);
+        }
     }
 }
